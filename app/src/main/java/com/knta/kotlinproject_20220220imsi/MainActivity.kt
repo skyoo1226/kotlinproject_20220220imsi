db3adb05114609f333de48167c7f9a1e47c2ab9b
sk.you1226 <you1226@naver.com>
2022-02-20 13:17:48 +0900

    Initial commit

---
+++ app/src/main/java/com/knta/kotlinproject_20220220imsi/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.knta.kotlinproject_20220220imsi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
